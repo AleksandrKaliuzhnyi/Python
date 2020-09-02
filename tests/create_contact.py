82bb75aca00e2affdbdc93f92a8a0fb9786ade5b
Oleksandr.Kaliuzhnyi <aleksandr.kaliuzhnyi@gmail.com>
2020-09-02 14:32:02 +0300

    HW-6

--- tests/create_contact.py
+++ tests/create_contact.py
@@ -1,19 +1,12 @@
-import pytest
 from model.contact import Contact
-from fixture.application_contact import Application_contact
-
-
-@pytest.fixture
-def app(request):
-    fixture = Application_contact()
-    request.addfinalizer(fixture.destroy)
-    return fixture
+import time
 
 def test_create_contact(app):
     app.session.login("admin", "secret")
     app.contact.create_contact(Contact("Oleksandr", "Kaliuzhnyi", "emkiss", "Infopulse", "Kiev", "0931234567", "QA",
                             "test@gmail.com", "7", "December", "1994"))
     app.session.logout()
+    time.sleep(0.5)
 
 def test_create_empty_contact(app):
     app.session.login("admin", "secret")
--- tests/create_group.py
+++ tests/create_group.py
@@ -1,18 +1,11 @@
-import pytest
 from model.group import Group
-from fixture.application_group import Application_group
-
-
-@pytest.fixture
-def app(request):
-    fixture = Application_group()
-    request.addfinalizer(fixture.destroy)
-    return fixture
+import time
 
 def test_create_group(app):
     app.session.login( username="admin", password="secret")
     app.group.create(Group(name="1st group", header="logo", footer="comment"))
     app.session.logout()
+    time.sleep(0.5)
 
 def test_create_empty_group(app):
     app.session.login( username="admin", password="secret")
