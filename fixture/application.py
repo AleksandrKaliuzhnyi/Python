5d8d3904e35a50c8e903bb55f8314eb872cc41be
Oleksandr.Kaliuzhnyi <aleksandr.kaliuzhnyi@gmail.com>
2020-09-17 14:22:44 +0300

    HW-16

--- fixture/application.py
+++ fixture/application.py
@@ -7,16 +7,24 @@ from fixture.conf_alert import ConfAlert
 
 class Application:
 
-    def __init__(self):
-        self.wd = webdriver.Firefox()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
         self.conf_alert = ConfAlert(self)
+        self.base_url = base_url
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
+        wd.get(self.base_url)
 
     def is_valid(self):
         try:
