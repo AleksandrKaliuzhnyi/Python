ffe4f8de6a625eb1dcded142c6514f930f3ec580
Oleksandr.Kaliuzhnyi <aleksandr.kaliuzhnyi@gmail.com>
2020-09-18 11:25:18 +0300

    HW-18,19

---
+++ data/contacts.py
@@ -0,0 +1,11 @@
+from model.contact import Contact
+
+
+testdata = [
+    Contact(firstname="Oleksandr", lastname="Kaliuzhnyi", nickname="emkiss", company="Infopulse", address="Kiev",
+            mobile="0931234567", work="QA",
+            email="test@gmail.com", bday="7", bmonth="December", byear="1994"),
+    Contact(firstname="Oleksandr1", lastname="Kaliuzhnyi1", nickname="emkiss1", company="Infopulse1", address="Kiev1",
+            mobile="0931234567", work="QA1",
+            email="1test@gmail.com", bday="7", bmonth="December", byear="1994"),
+]
--- tests/create_contact.py
+++ tests/create_contact.py
@@ -1,34 +1,9 @@
 from model.contact import Contact
 import time
-import random
-import string
-import pytest
 
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-def random_day():
-    return str(random.randint(1, 31))
-
-def random_month():
-    months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
-              "November", "December"]
-    return str(random.choice(months))
-
-def random_year():
-    return str(random.randint(1, 9999))
-
-testdata = [Contact(firstname="", lastname="", nickname="")] + [
-    Contact(firstname=random_string("firstname", 12), lastname=random_string("lastname", 12), nickname=random_string("nickname", 12), company=random_string("company", 12),
-            address=random_string("address", 12), mobile=random_string("mobile", 10), work=random_string("work", 10), email=random_string("email", 10),
-            bday=random_day(), bmonth=random_month(), byear=random_year())
-    for i in range(5)
-]
-
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_create_contact(app, contact):
+def test_create_contact(app, json_contacts):
+    contact = json_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
